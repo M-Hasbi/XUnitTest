3428e06af1a8d53342c11babcc39d9356f11d024
M-Hasbi <kaynakhasbi1@gmail.com>
2021-11-29 14:35:49 +0100

    create : Contains , DoesNotContains and Equals Methods

--- XUnitTest.Test/CalculatorTest.cs
+++ XUnitTest.Test/CalculatorTest.cs
@@ -13,19 +13,25 @@ namespace XUnitTest.Test
         [Fact]
         public void AddTest()
         {
-            //Arrange --> initiliazing the required paramethers 
+            ////Arrange --> initiliazing the required paramethers 
 
-            int a = 5;
-            int b = 20;
-            var calculater = new Calculator();
+            //int a = 5;
+            //int b = 20;
+            //var calculater = new Calculator();
 
-            //Act --> calling the method which is going to be tested
-            var total = calculater.Add(a, b);
+            ////Act --> calling the method which is going to be tested
+            //var total = calculater.Add(a, b);
 
 
 
-            //Assert --> the testing method
-            Assert.Equal<int>(25, total);
+            ////Assert --> the testing method
+            //Assert.Equal<int>(25, total);
+
+            //var names = new List<string>() { "Mehmet", "Hasbi", "Kaynak" };
+            //Assert.Contains(names, x => x == "Kaynak");
+
+            //Assert.Contains("Hasbi","Mehmet Hasbi Kaynak");
+            //Assert.DoesNotContain("Said", "Mehmet Hasbi Kaynak");
         }
 
     }
